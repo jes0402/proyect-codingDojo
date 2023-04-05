30b2e0abc3bb64a81f9e2228c73df1668fa35be4
jes0402 <yesiik.97@gmail.com>
2023-04-05 11:40:55 -0700

    commit

--- server.py
+++ server.py
@@ -4,12 +4,12 @@ import os
 import stripe
 
 
-stripe_keys = {
+""" stripe_keys = {
     "secret_key": os.environ["STRIPE_SECRET_KEY"],
     "publishable_key": os.environ["STRIPE_PUBLISHABLE_KEY"],
 }
 
-stripe.api_key = stripe_keys["secret_key"]
+stripe.api_key = stripe_keys["secret_key"] """
 
 @app.route("/config")
 def get_publishable_key():
