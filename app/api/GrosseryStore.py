7fd04eb38f27e82c52431030e32bbf69d7e974cd
Ignacio Labarca <ignaciolabarca@iMac-de-Ignacio.local>
2023-04-10 09:00:48 -0400

    cambios logica

--- app/api/GrosseryStore.py
+++ app/api/GrosseryStore.py
@@ -1,33 +1,16 @@
-# import time 
-
-# from selenium import webdriver 
-# from selenium.webdriver import Chrome 
-# from selenium.webdriver.chrome.service import Service 
-# from selenium.webdriver.common.by import By 
-# from webdriver_manager.chrome import ChromeDriverManager
-
-# # start by defining the options 
-# options = webdriver.ChromeOptions() 
-# # we don't need it as the page also populated with the running javascript code. 
-# options.page_load_strategy = 'none' 
-# # this returns the path web driver downloaded 
-# chrome_path = ChromeDriverManager().install() 
-# chrome_service = Service(chrome_path) 
-# # pass the defined options and service objects to initialize the web driver 
-# driver = Chrome(options=options, service=chrome_service) 
-# driver.implicitly_wait(5)
+import requests
+from bs4 import BeautifulSoup
 
 
+URL = "https://veggo.cl/tomate-kilogramo.html"
 
-# def get_the_price_by_kg(url):
-#     driver.get(url)
-#     time.sleep(5)
-#     price = driver.find_element(By.CSS_SELECTOR, "div[class*='price-per-um__pdp'")
-#     if price:
-#         price = price.text
-#         price = ''.join(filter(str.isdigit, price))
-#         price = int(price)
-#     else:
-#         price = "not found"
-#     driver.quit()
-#     return price
+def get_price(URL):
+
+    page = requests.get(URL)
+
+    soup = BeautifulSoup(page.content, "html.parser")
+    price = soup.find("span", {"class": "price"}).text
+
+    priceInt = int(price.replace("$", "").replace(".", ""))
+
+    return priceInt
---
+++ app/api/VariablePrices.py
@@ -0,0 +1,25 @@
+import decimal
+
+
+
+def getPriceSize(size):
+    pequena = decimal.Decimal(10.00)
+    mediana = decimal.Decimal(13.00)
+    grande = decimal.Decimal(15.00)
+
+    if size == "Small":
+        return pequena
+    if size == "Medium":
+        return mediana
+    if size == "Large":
+         return grande
+    return 0
+        
+def getPriceCrust(crust):
+    thick = decimal.Decimal(1.5)
+    thin = decimal.Decimal(0.5)   
+    if crust == "Thick Crust":
+            return thick
+    if crust == "Thin Crust":
+            return thin
+    return 0
